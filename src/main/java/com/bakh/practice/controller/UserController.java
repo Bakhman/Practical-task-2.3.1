d339fbc7b5cf22cd8ee9c71295d1b06d028a5074
Bakhman <begaev7@gmail.com>
2022-07-14 19:08:23 +0300

    Add files via upload

---
+++ src/main/java/com/bakh/practice/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.bakh.practice.controller;
+
+import com.bakh.practice.model.User;
+import com.bakh.practice.service.UserService;
+import com.bakh.practice.service.UserServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Bakhmai Begaev
+ */
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService = new UserServiceImpl();
+
+
+    @GetMapping()
+    public String getAllUsers(Model model) {
+        model.addAttribute("userList", userService.getAllUsers());
+        return "users";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView editPage(@PathVariable("id") int id) {
+        User user = userService.getUserById(id);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public ModelAndView editUser(@ModelAttribute("user") User user){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/users");
+        userService.editUser(user);
+        return  modelAndView;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView addPage() {
+        User user = new User();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addUser");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ModelAndView addUser(@ModelAttribute("user") User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/users");
+        userService.add(user);
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        userService.removeUserById(id);
+        return "redirect:/users";
+    }
+
+}
